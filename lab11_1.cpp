4bd46adfea883b7e92fdcc5ff1ecfb490f769382
krit <krittapasboonruang@gmail.com>
2026-01-06 15:25:27 +0700

    lab11_1 complete

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,19 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(){
+    cout << "Press Enter 3 times to reveal your future.\n";
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    string grades[] = {"A", "B" , "B+", "C+", "C", "D+", "D", "F", "W"};
+    int randomIndex = rand() % 9;
+    string selectedGrade = grades[randomIndex];
+    cout << "\n";
+    cout << "You will get " << selectedGrade << " in this 261102.";
+    
+    return 0;
+}
